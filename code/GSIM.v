3ab1bee525fa9e81521bdee6a103a97ac1a6c743
ray13542 <ray13542@gmail.com>
2024-04-24 18:07:38 +0800

    Pass all 5 testbench

--- code/GSIM.v
+++ code/GSIM.v
@@ -4,11 +4,11 @@ input   clk ;
 input   reset ;
 input   in_en;
 output  out_valid;
-input   [15:0]  b_in;
+input signed  [15:0]  b_in;
 output  [31:0]  x_out;
 
 
-parameter ITERATION  = 0;
+parameter ITERATION  = 120;
 
 parameter state_INPUT = 3'd0;
 parameter state_INIT  = 3'd1;
@@ -32,6 +32,8 @@ reg [3:0] b_addr, nxt_b_addr;
 reg [6:0] iter, nxt_iter;
 wire signed [31:0] x_result;
 wire PE_valid, PE_en;
+// reg signed [3:0] x_tmp [0:15];
+
 //--------------------------------------
 // Finite State Machine
 //--------------------------------------
@@ -94,19 +96,19 @@ always @(*) begin
     case (state)
         state_INIT: begin // initialize x values = b/20
             for (i = 0;i < 16 ; i = i +1) begin
-                nxt_x[i] = {{6{b[i][31]}}, b[i][31:6]}+ {{5{b[i][31]}}, b[i][31:5]}+ {{9{b[i][31]}}, b[i][31:9]}+ {{10{b[i][31]}}, b[i][31:10]};
-                $display("x[",i+1,"] = %h",nxt_x[i]);
+                nxt_x[i] = b[i];
             end
+            
         end
         state_UPDATE:begin // input result of PE to x
             for (i = 0;i < 16 ; i = i +1) begin
                 nxt_x[i] = (x_addr == i)? x_result: x[i];
-                
+                // $display("x[",i+1,"] = %h", nxt_x[i]);
             end
         end
         state_XDONE: begin
             for (i = 0;i < 16 ; i = i +1) begin
-                $display("x[",i+1,"] = %h",x[i]);
+                // $display("x[",i+1,"] = %h",x[i]);
                 nxt_x[i] = x[i];
             end
         end
@@ -118,6 +120,7 @@ always @(*) begin
     endcase
 end
 always @(posedge clk) begin
+    // $display("%d", iter);
     if (reset) begin
         for (i =0 ; i < 16; i = i + 1) begin
             x[i] <= 0;
@@ -142,6 +145,7 @@ always @(*) begin
         end
         state_OUTPUT: begin // output x_addr for output
             nxt_x_addr = x_addr + 1;
+            // for(i=0; i<16; i=i+1) $display("x[",i+1,"] = %h",x[i]);
         end
         default: nxt_x_addr = x_addr;
     endcase
@@ -243,14 +247,20 @@ module PE ( clk, reset, en, x1, x2, x3, x4, x5, x6, b_in, x_out,  valid);
     // reg/net declaration
     //--------------------------------------
     reg [2:0] counter, nxt_counter;
-    wire signed [31:0] mul_13, mul_6, add_6;
-    reg signed [31:0] add_1, add_2, add_3, add_4, add_5, divide_20;
-    reg signed [31:0] nxt_add_1, nxt_add_2, nxt_add_3, nxt_add_4, nxt_add_5, nxt_divide_20;
+    wire signed [31:0] nxt_divide_20;
+    wire signed [36:0] mul_13;
+    wire signed [35:0] mul_6;
+    wire signed [36:0] add_6;
+    reg signed [32:0] add_4, nxt_add_4;
+    reg signed [36:0] add_5, nxt_add_5;
+    reg signed [32:0] add_1, add_2, add_3;
+    reg signed [31:0] divide_20;
+    reg signed [32:0] nxt_add_1, nxt_add_2, nxt_add_3;
     //--------------------------------------
     assign x_out = divide_20;
     assign valid = (counter == 3)? 1'b1: 1'b0;
-    assign mul_13=  {add_1[31], add_1[27:0], 3'b0} + {add_1[31], add_1[28:0], 2'b0}+ add_1;
-    assign mul_6 =  {add_2[31], add_2[28:0], 2'b0} + {add_2[31], add_2[29:0], 1'b0};
+    assign mul_13=  {add_1, 3'b0} + {add_1[32],add_1, 2'b0}+ {{4{add_1[32]}}, add_1};
+    assign mul_6 =  {add_2, 2'b0} + {add_2[32], add_2, 1'b0};
     assign add_6 =  add_5 + add_4;
     //--------------------------------------
     // counter
@@ -279,8 +289,16 @@ module PE ( clk, reset, en, x1, x2, x3, x4, x5, x6, b_in, x_out,  valid);
         nxt_add_4 = add_3 + b_in;
         nxt_add_5 = mul_13 - mul_6;
         // nxt_divide_20 = add_6>>6 + add_6>>5 + add_6>>9 + add_6>>10;
-        nxt_divide_20 = {{6{add_6[31]}}, add_6[31:6]}+ {{5{add_6[31]}}, add_6[31:5]}+ {{9{add_6[31]}}, add_6[31:9]}+ {{10{add_6[31]}}, add_6[31:10]};
+        
+        /*
+        nxt_divide_20 = {{ 5{add_6[32]}}, add_6[32: 5]}+  {{ 6{add_6[32]}}, add_6[32: 6]}+  {{ 9{add_6[32]}}, add_6[32: 9]}+ 
+                        {{10{add_6[32]}}, add_6[32:10]}+  {{13{add_6[32]}}, add_6[32:13]}+  {{14{add_6[32]}}, add_6[32:14]}+
+                        {{17{add_6[32]}}, add_6[32:17]}+  {{18{add_6[32]}}, add_6[32:18]}+  {{21{add_6[32]}}, add_6[32:21]}+
+                        {{22{add_6[32]}}, add_6[32:22]}+  {{25{add_6[32]}}, add_6[32:25]}+  {{26{add_6[32]}}, add_6[32:26]}+
+                        {{29{add_6[32]}}, add_6[32:29]}+  {{30{add_6[32]}}, add_6[32:30]};
+        */
     end
+    divide_by20 div17(.in_div(add_6), .res(nxt_divide_20));
     always @(posedge clk) begin
         case (counter)
             0: begin
@@ -297,4 +315,22 @@ module PE ( clk, reset, en, x1, x2, x3, x4, x5, x6, b_in, x_out,  valid);
             end
         endcase
     end
+endmodule
+
+module divide_by20(in_div, res);
+    input signed [36:0] in_div;
+    output signed [31:0] res;
+    parameter ACCU = 43;
+    wire [ACCU : 0] tmp;
+    wire [ACCU - 38:0] res_tmp1;
+    wire [4:0] res_tmp2;
+    assign tmp = in_div << (ACCU - 37);
+    assign {res_tmp2, res, res_tmp1} =  {{ 5{tmp[ACCU]}}, tmp[ACCU: 5]}+  {{ 6{tmp[ACCU]}}, tmp[ACCU: 6]}+  {{ 9{tmp[ACCU]}}, tmp[ACCU: 9]}+ 
+                                        {{10{tmp[ACCU]}}, tmp[ACCU:10]}+  {{15{tmp[ACCU]}}, tmp[ACCU:13]}+  {{14{tmp[ACCU]}}, tmp[ACCU:14]}+
+                                        {{17{tmp[ACCU]}}, tmp[ACCU:17]}+  {{18{tmp[ACCU]}}, tmp[ACCU:18]}+  {{21{tmp[ACCU]}}, tmp[ACCU:21]}+
+                                        {{22{tmp[ACCU]}}, tmp[ACCU:22]}+  {{25{tmp[ACCU]}}, tmp[ACCU:25]}+  {{26{tmp[ACCU]}}, tmp[ACCU:26]}+
+                                        {{29{tmp[ACCU]}}, tmp[ACCU:29]}+  {{30{tmp[ACCU]}}, tmp[ACCU:30]}+  {{33{tmp[ACCU]}}, tmp[ACCU:33]}+
+                                        {{34{tmp[ACCU]}}, tmp[ACCU:34]}+  {{34{tmp[ACCU]}}, tmp[ACCU:34]}+  {{37{tmp[ACCU]}}, tmp[ACCU:37]};
+
+
 endmodule
